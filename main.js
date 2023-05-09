7ba2f24ab74f9f936465b28615ecdf82ae2e619d
example <tranhobao0914705078@gmail.com>
2023-05-09 18:01:31 +0700

    oke

---
+++ main.js
@@ -0,0 +1,17 @@
+const box1 = document.querySelector('#box1');
+const box2 = document.querySelector('#box2');
+
+box1.addEventListener('click', () => {
+    box2.classList.toggle('slide-down');
+});
+
+const myInput = document.getElementById('myInput');
+const charCount = document.getElementById('charCount');
+
+  myInput.addEventListener('input', function() {
+    if(myInput.value.length > 200){
+      alert('Vượt quá ký tự cho phép!!!');
+    }else{
+      charCount.textContent = `${myInput.value.length}/200`;
+    }
+});
